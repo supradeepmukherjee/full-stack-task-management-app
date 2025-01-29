29dd1a756500c4cdfbe3f0ec7b5a597bda237115
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-01-29 14:26:08 +0530

    Created User Schema

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import { hash } from 'bcrypt'
+import mongoose, { model, Schema } from 'mongoose'
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+        minLength: 8
+    },
+})
+
+schema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = await hash(this.password, 10)
+})
+
+export const User = mongoose.models.FoodUser || model('FoodUser', schema) //  I am naming it FoodUser because another model named 'User' is already in use.
